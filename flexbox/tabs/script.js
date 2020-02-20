50e9818be40b2ccef89ff649fba93cf198f018ab
yurifyodorov <yuri.inpost@yandex.ru>
2020-02-20 15:35:06 +0300

    add Tabs

---
+++ flexbox/tabs/script.js
@@ -0,0 +1,7 @@
+var $tabs = $('.tabs .tab');
+
+$tabs.click(function(event) {
+  var selectedClass = 'is-tab-selected';
+  $tabs.removeClass(selectedClass);
+  $(event.target).addClass(selectedClass);
+});
